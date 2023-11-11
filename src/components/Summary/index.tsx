c89e6c58574eb93e2d1226d70781c59b1b7e360b
Jean Carlo Oliveira <jcjeancarloo@gmail.com>
2023-11-11 18:31:52 -0300

    fix: add grid to summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,17 +2,17 @@ import Card from '@components/Common/Card'
 
 const Summary = () => {
   return (
-    <div className="flex flex-col gap-y-2 md:flex-row  bg-red-400 w-full">
-      <Card>
+    <div className="flex flex-col gap-y-2 gap-x-4 md:flex-row  bg-red-400 w-full">
+      <Card width="md:w-1/4">
         <h1>Content here</h1>
       </Card>
-      <Card>
+      <Card width="md:w-1/4">
         <h1>Content here</h1>
       </Card>
-      <Card>
+      <Card width="md:w-1/4">
         <h1>Content here</h1>
       </Card>
-      <Card>
+      <Card width="md:w-1/4">
         <h1>Content here</h1>
       </Card>
     </div>
