073ef3d89992b2e973a8b1719f681d606efd64ec
Jean Carlo Oliveira <jcjeancarloo@gmail.com>
2023-11-12 15:56:42 -0300

    feat: add area chart

---
+++ src/components/Charts/Area/index.tsx
@@ -0,0 +1,53 @@
+import Chart from 'react-apexcharts'
+
+type DonutProps = {
+  data: any
+}
+
+const settings = {
+  options: {
+    chart: {
+      foreColor: '#64748b',
+      toolbar: {
+        show: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: 'smooth' as const,
+    },
+    xaxis: {
+      type: 'datetime' as const,
+      categories: [
+        '2018-09-19T00:00:00.000Z',
+        '2018-09-19T01:30:00.000Z',
+        '2018-09-19T02:30:00.000Z',
+        '2018-09-19T03:30:00.000Z',
+        '2018-09-19T04:30:00.000Z',
+        '2018-09-19T05:30:00.000Z',
+        '2018-09-19T06:30:00.000Z',
+      ],
+    },
+    tooltip: {
+      x: {
+        format: 'dd/MM/yy HH:mm',
+      },
+    },
+  },
+}
+
+const Area = ({ data }: DonutProps) => {
+  return (
+    <Chart
+      options={settings.options}
+      series={data.series}
+      type="area"
+      width={'100%'}
+      height={'100%'}
+    />
+  )
+}
+
+export default Area
--- src/components/Overview/index.tsx
+++ src/components/Overview/index.tsx
@@ -1,30 +1,37 @@
+import Area from '@components/Charts/Area'
+import Donut from '@components/Charts/Donut'
 import Card from '@components/Common/Card'
 import Table from '@components/Table'
-import Donut from '../Charts/Donut'
 
 const donutData = { series: [44, 55, 41, 17, 15] }
+const areaData = {
+  series: [
+    {
+      name: 'series1',
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    // {
+    //   name: 'series2',
+    //   data: [11, 32, 45, 32, 34, 52, 41],
+    // },
+  ],
+}
 
 const Overview = () => {
   return (
     <div className="w-full h-[1024px] flex flex-col md:flex-row gap-x-4 gap-y-4">
       <div className="h-full flex flex-col gap-y-4 md:w-[70%] w-full">
-        <Card width="w-full" height="md:h-1/2 h-full">
+        <Card width="w-full" height="h-full">
           <div className="flex flex-col gap-y-4 h-full">
             <h1 className="text-2xl">Tabela 1</h1>
             <Table />
           </div>
         </Card>
-        <Card height="h-1/2">
-          <div className="bg-green-200 w-full">
-            <h1>Grafico 2</h1>
-          </div>
-        </Card>
       </div>
       <div className="h-full flex flex-col gap-y-4 md:w-[30%] w-full">
         <Card height="h-1/2">
-          <div className="bg-orange-200 w-full">
-            <h1>Grafico3</h1>
-          </div>
+          <h1 className="text-2xl">Grafico 1</h1>
+          <Area data={areaData} />
         </Card>
         <Card height="md:h-1/2 h-full">
           <h1 className="text-2xl">Grafico 1</h1>
