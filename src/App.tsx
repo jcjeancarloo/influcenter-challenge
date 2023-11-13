fd9641a8fc32a07973a207ea7c35c19ebaa247a2
Jean Carlo Oliveira <jcjeancarloo@gmail.com>
2023-11-13 20:04:55 -0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Suspense, lazy, useEffect } from 'react'
+import { PuffLoader } from 'react-spinners'
+
 import Footer from '@components/Footer'
 import Header from '@components/Header'
-import Overview from '@components/Overview'
-import Summary from '@components/Summary'
 import Wrapper from '@components/Wrapper'
+
 import useProducts from '@hooks/useProducts'
 import useUsers from '@hooks/useUsers'
-import { useEffect } from 'react'
+
+const Overview = lazy(() => import('@components/Overview'))
+const Summary = lazy(() => import('@components/Summary'))
 
 function App() {
   const { totalUsers, fetchUsers, parsedUsers, uploadUser } = useUsers()
@@ -26,29 +30,37 @@ function App() {
 
   return (
     <main className="flex flex-col justify-center items-center w-full min-h-screen h-full bg-[#1e1e1e] font-nunito">
-      <Wrapper>
-        <Header />
-        <Summary
-          totalUsers={totalUsers}
-          totalProducts={totalProducts}
-          totalCategories={totalCategories}
-          totalSales={totalSales}
-          handleAddSale={handleAddSale}
-        />
-        <Overview
-          users={parsedUsers}
-          products={products}
-          uploadUser={uploadUser}
-          uploadProduct={uploadProduct}
-          chartData={{
-            totalUsers,
-            totalProducts,
-            totalCategories,
-            totalSales,
-          }}
-        />
-        <Footer />
-      </Wrapper>
+      <Suspense
+        fallback={
+          <div>
+            <PuffLoader color="white" />
+          </div>
+        }
+      >
+        <Wrapper>
+          <Header />
+          <Summary
+            totalUsers={totalUsers}
+            totalProducts={totalProducts}
+            totalCategories={totalCategories}
+            totalSales={totalSales}
+            handleAddSale={handleAddSale}
+          />
+          <Overview
+            users={parsedUsers}
+            products={products}
+            uploadUser={uploadUser}
+            uploadProduct={uploadProduct}
+            chartData={{
+              totalUsers,
+              totalProducts,
+              totalCategories,
+              totalSales,
+            }}
+          />
+          <Footer />
+        </Wrapper>
+      </Suspense>
     </main>
   )
 }
