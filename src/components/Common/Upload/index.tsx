e9d5b8d18aad6389c0c0e74043a5613b271c6f3c
Jean Carlo Oliveira <jcjeancarloo@gmail.com>
2023-11-13 13:16:55 -0300

    feat: add icon to add button

--- src/components/Common/Upload/index.tsx
+++ src/components/Common/Upload/index.tsx
@@ -1,3 +1,5 @@
+import { FaPlus } from 'react-icons/fa'
+
 type UploadProps = {
   description: string
   handleUpload: (e: React.ChangeEvent<HTMLInputElement>) => void
@@ -6,8 +8,9 @@ type UploadProps = {
 const Upload = ({ handleUpload, description }: UploadProps) => (
   <label
     htmlFor="jsonFileInput"
-    className="bg-[#44a5ff] text-white py-2 px-4 rounded cursor-pointer"
+    className="bg-emerald-500 hover:bg-emerald-400 transition text-white py-2 px-4 rounded cursor-pointer flex flex-row items-center gap-x-2"
   >
+    <FaPlus className="text-md" />
     {description}
     <input
       type="file"
